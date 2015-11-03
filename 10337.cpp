a9ab38a217effc6077b3bcfe33bea5a5ac14444f
Raymond van Venetie <rayvanve@gmail.com>
2015-11-03 19:04:24 +0100

    Joepie

---
+++ 10337.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+#define LL long long
+#define constant value
+using namespace std;
+
+int wind[1003][11]
+
+int main(){
+  int N;
+  cin >> N;
+  for (int i =0 ; i < N; i++) {
+    int X;
+    cin >> X;
+
+
+
+  }
+  return 0;
+}
+
+
