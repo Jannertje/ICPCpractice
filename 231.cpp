41d34c659c888899451e11da8d826a7148c4b9d4
Jan Westerdiep <janner@gmail.com>
2015-11-09 21:49:15 +0100

    it was a fricking output error. derp me. also added super short LIS implementation

--- 231.cpp
+++ 231.cpp
@@ -7,10 +7,21 @@
 #include <cmath>
 #include <cstdlib>
 
+/* Supershort LIS
+    vector<int> seq;                                                //fill this
+    vector<int> cnt(seq.size(), 1);                //initialize memo table at 1
+    for( int i = 0; i < seq.size(); i++) //DP recur rule: LIS(i) = 1+max(LIS(j))
+      for( int j = 0; j < i; j++)                         // for j < i, Xj < Xi
+        if( seq[j] < seq[i] && cnt[i] < cnt[j]+1 )    //longest inc subsequence
+          cnt[i] = cnt[j] + 1;
+    int max = *max_element( cnt.begin(), cnt.end());    //final opt: max LIS(j)
+ */
+
 using namespace std;
 
 int main() {
   int N;
+  int i = 1;
   while( (cin >> N) && N > -1) {
     vector<int> seq;
     seq.push_back(N);
@@ -22,10 +33,11 @@ int main() {
     vector<int> cnt(seq.size(), 1);                //initialize memo table at 1
     for( int i = 0; i < seq.size(); i++)    
       for( int j = 0; j < i; j++)
-        if( seq[j] >= seq[i])               //longest nonincreasing subsequence
-          cnt[i] = max( cnt[i], cnt[j]+1);                //recurrence rule #2
+        if( seq[j] >= seq[i] && cnt[i] < cnt[j]+1 ) //longest nonincreasing subsequence
+          cnt[i] = cnt[j] + 1;
 
-    cout << *max_element( cnt.begin(), cnt.end()) << endl;
+    if( i != 1) cout << endl;
+    cout << "Test #" << i++ << ":" << endl << "  maximum possible interceptions: " << *max_element( cnt.begin(), cnt.end()) << endl;
   }
   return 0;
 }
