871c2be48a35c26db05ae53544b629033f857de6
Jan Westerdiep <janner@gmail.com>
2015-11-21 15:25:44 +0100

    yet _another_ partial solution

--- SWERC2011/G.cpp
+++ SWERC2011/G.cpp
@@ -8,29 +8,31 @@
 #include <cstdlib>
 
 
-#define INF 2000000
+#define INF 2000000000
 using namespace std;
 
 int main() {
   int N;
   while( cin >> N && N > 0) {
     int A[N];
-    int acc[N+1], leastl[N+1], leasth[N+1];
-    acc[0] = 0; leastl[0] = INF;
-    for( int n = 0; n < N; n++) {
+    int acc[N], leastl[N], leasth[N];
+    cin >> A[0];
+    acc[0] = A[0];
+    leastl[0] = acc[0];
+    for( int n = 1; n < N; n++) {
       cin >> A[n];
-      acc[n+1] = A[n] + acc[n];
-      leastl[n+1] = min(leastl[n], acc[n]);
+      acc[n] = A[n] + acc[n-1];
+      leastl[n] = min(leastl[n-1], acc[n]);
     }
-    leasth[N] = INF;
-    for( int n = N-1; n >= 0; n--) {
+    leasth[N-1] = acc[N-1];
+    for(int n = N-2; n >= 0; n--) {
       leasth[n] = min(leasth[n+1], acc[n]);
     }
 
     int cnt = 0;
     for( int n = 0; n < N; n++) {
-      if( acc[n+1] >= leasth[n]) {
-        if( acc[N] - acc[n] >= leastl[n]) {
+      if( leasth[n] >= (n == 0 ? 0 : acc[n-1])) {
+        if( acc[N] - (n==0?0:acc[n-1]) >= -leastl[n+1]) {
           cnt++;
         }
       }
